6ccdc5c0e477f411c6a7d19fe1a2c2ba153a60f9
Abdul Alhumidan <100884382+Abdul88Canada@users.noreply.github.com>
2024-11-29 21:08:20 +0300

    init client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './assets/css/styles.css';
+import Index from './components/index';
+
+function App() {
+  return (
+    <div className="App">
+      <Index />
+    </div>
+  );
+}
+
+export default App;
